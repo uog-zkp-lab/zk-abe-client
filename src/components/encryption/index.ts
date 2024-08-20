2f1d94cf4c07339ef3eb101593f9e87ad0852084
CHUNG, Fu Chuan <2941673c@student.gla.ac.uk>
2024-08-20 22:31:42 +0200

    feat: added a index to output all components

---
+++ src/components/encryption/index.ts
@@ -0,0 +1,6 @@
+import CreateToken from './CreateToken'
+import DataEncrypt from './DataEncrypt'
+import IPFSUpload from './IPFSUpload'
+import PublicKeyRequest from './PublicKeyRequest'
+
+export { CreateToken, DataEncrypt, IPFSUpload, PublicKeyRequest }
