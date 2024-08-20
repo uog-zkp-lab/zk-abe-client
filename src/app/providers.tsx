ca2e4f6fe6208af863f2f2c06bc5321d01412e96
CHUNG, Fu Chuan <2941673c@student.gla.ac.uk>
2024-08-20 22:25:36 +0200

    fix: delete helia provider due to multiple error

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { HeliaProvider } from './heliaProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { type ReactNode, useState } from 'react'
 import { type State, createConfig, http, WagmiProvider } from 'wagmi'
@@ -8,48 +7,53 @@ import { arbitrumSepolia } from 'wagmi/chains'
 import { getConfig } from '@/wagmi_helper'
 import { Navbar, Footer } from '@/components/navigation'
 import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
-import { Box } from '@mui/material';
+import { Box } from '@mui/material'
 
 export const config = createConfig(
-  getDefaultConfig({
+    getDefaultConfig({
+        chains: [arbitrumSepolia],
+        transports: {
+            [arbitrumSepolia.id]: http(
+                `https://arb-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+            ),
+        },
 
-    chains: [arbitrumSepolia],
-    transports: {
-        [arbitrumSepolia.id]: http(
-          `https://arb-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`),
-    },
+        walletConnectProjectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? '',
+        appName: 'Decentralized ZKABE System',
 
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID ?? '',
-    appName: "Decentralized ZKABE System",
-
-    appDescription: "This is a Decentralized ABE System leveraging zero knowledge virtual machine to preserve the privacy of data processor",
-    appUrl: "https://family.co",
-    appIcon: "https://family.co/logo.png",
-  }),
-);
+        appDescription:
+            'This is a Decentralized ABE System leveraging zero knowledge virtual machine to preserve the privacy of data processor',
+        appUrl: 'https://family.co',
+        appIcon: 'https://family.co/logo.png',
+    }),
+)
 
 export function Providers(props: {
-  children: ReactNode
-  initialState?: State
+    children: ReactNode
+    initialState?: State
 }) {
-  const [config] = useState(() => getConfig())
-  const [queryClient] = useState(() => new QueryClient())
+    const [config] = useState(() => getConfig())
+    const [queryClient] = useState(() => new QueryClient())
 
-  return (
-    <WagmiProvider config={config} initialState={props.initialState}>
-      <QueryClientProvider client={queryClient}>
-            <ConnectKitProvider>
-                <HeliaProvider>
-                <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-                        <Navbar />
-                        <Box component="main" sx={{ flexGrow: 1 }}>
-                            {props.children}
+    return (
+        <WagmiProvider config={config} initialState={props.initialState}>
+            <QueryClientProvider client={queryClient}>
+                <ConnectKitProvider>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                minHeight: '100vh',
+                            }}
+                        >
+                            <Navbar />
+                            <Box component="main" sx={{ flexGrow: 1 }}>
+                                {props.children}
+                            </Box>
+                            <Footer />
                         </Box>
-                        <Footer />
-                </Box>
-                </HeliaProvider>
-            </ConnectKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  )
+                </ConnectKitProvider>
+            </QueryClientProvider>
+        </WagmiProvider>
+    )
 }
