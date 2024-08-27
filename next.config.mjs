7dff1ce241f94ff8a05d3f94182877a436e23bda
CHUNG, Fu Chuan <2941673c@student.gla.ac.uk>
2024-08-27 12:54:40 +0100

    fix: get fid of warning

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    transpilePackages: ['supports-color'],
+    experimental: {
+        esmExternals: 'loose',
+    },
+}
 
 export default nextConfig
